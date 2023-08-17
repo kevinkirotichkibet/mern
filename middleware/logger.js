0aa2498e3c25162ab77791194ab5919d9b458f82
Kevin Kirotich Kibet <kirotichkevin@gmail.com>
2023-08-17 14:51:50 +0300

    Mongoose setup

--- middleware/logger.js
+++ middleware/logger.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-const logEvents = async (message, logFileName) => {
+  const logEvents = async (message, logFileName) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
